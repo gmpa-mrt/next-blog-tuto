f08c46e099acfcaf04235f800d7839f1e1684a1c
Meurant <gaetanmeurant@gmail.com>
2021-12-10 02:48:01 +0100

    public folder with favicon.ico

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import NavBar from "../components/NavBar";
 import '../styles/globals.css'
+import Head from "next/head";
 
 function App({Component, pageComponents}) {
     return (
         <>
+            <Head>
+                <link rel="icon" href="/favicon.ico"/>
+                <title>My Blog</title>
+            </Head>
             <header>
                 <NavBar/>
             </header>
