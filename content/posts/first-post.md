735ab77f0b0b7bfe70465e1fc9b4e4186e020857
Meurant <gaetanmeurant@gmail.com>
2021-12-11 03:19:28 +0100

    sub-path + markdown

---
+++ content/posts/first-post.md
@@ -0,0 +1,11 @@
+#First Post 
+
+This my first post, written in MarkDown
+
+Here's some _italic text_.
+
+and a list :
+
+* one
+* two
+* three
---
+++ pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function FirstPost() {
+    return (
+        <>
+            <Head>
+                <title>First Post - My blog</title>
+            </Head>
+            <main>
+                <h1>First Post</h1>
+            </main>
+        </>
+    )
+}
+
+export default FirstPost
