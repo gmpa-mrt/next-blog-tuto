06fc1982c89fded169dc2959b01d042bbd927529
Meurant <gaetanmeurant@gmail.com>
2021-12-14 15:48:01 +0100

    Using Node.js readfile from internal json

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
+import {readFile} from "node:fs/promises"
 import Head from "next/head";
 
-function FirstPost({ post }) {
+function FirstPost({post}) {
     console.log(post)
     return (
         <>
@@ -18,12 +19,9 @@ function FirstPost({ post }) {
 export default FirstPost
 
 export async function getStaticProps() {
+    const data = await readFile('content/posts/first-post.json', 'utf-8')
+    const post = JSON.parse(data)
     return {
-        props: {
-            post: {
-                title: 'My title',
-                content: 'My content'
-            }
-        }
+        props: {post}
     }
 }
