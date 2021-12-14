38e4caf398c022580c0bb68d72388ee5967c0fc5
Meurant <gaetanmeurant@gmail.com>
2021-12-14 15:56:38 +0100

    Lib folder + encapsulation

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,4 @@
-import {readFile} from "node:fs/promises"
+import {getPost} from "../../lib/posts";
 import Head from "next/head";
 
 function FirstPost({post}) {
@@ -19,8 +19,7 @@ function FirstPost({post}) {
 export default FirstPost
 
 export async function getStaticProps() {
-    const data = await readFile('content/posts/first-post.json', 'utf-8')
-    const post = JSON.parse(data)
+    const post = await getPost('first-post')
     return {
         props: {post}
     }
