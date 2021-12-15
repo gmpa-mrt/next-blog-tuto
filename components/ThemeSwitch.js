9c0ad6f0d7d6f663c2d6624daf8512120396883c
Meurant <gaetanmeurant@gmail.com>
2021-12-15 12:58:45 +0100

    ThemeBtn + localStorage

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,14 +1,27 @@
 import {useState} from "react";
 import DarkTheme from "./DarkTheme";
 
+function loadThemeMode() {
+    if (typeof localStorage === 'undefined') {
+        return false;
+    }
+    const value = localStorage.getItem('themeMode')
+    return (value === null) ? false : JSON.stringify(value)
+}
+
 const ThemeSwitch = () => {
 
-    const [themeMode, setThemeMode] = useState(false)
+    const [themeMode, setThemeMode] = useState(loadThemeMode)
     const text = themeMode ? "Light Mode" : "Dark Mode"
+
+    const handleClick = () => {
+        localStorage.setItem('themeMode', JSON.stringify(!themeMode))
+        setThemeMode(!themeMode)
+    }
 
     return (
         <>
-            <button onClick={() => setThemeMode(!themeMode)}>
+            <button onClick={handleClick} suppressHydrationWarning>
                 {text}
             </button>
             <style jsx>{`
